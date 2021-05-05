96b20650058fbd417578cc84061a5c00a65527c1
baiken314 <60582899+baiken314@users.noreply.github.com>
2021-05-04 23:23:23 -0400

    updated garden api routes

--- routes/gardenRouter.js
+++ routes/gardenRouter.js
@@ -11,4 +11,83 @@ router.route("/").get(async (req, res) => {
     );
 });
 
+/**
+ * req.body.user: User._id  // the person making the post
+ * req.body.number: Number
+ * req.body.gardener: User._id  // optional for assigning
+ */
+router.route("/create").post(async (req, res) => {
+    console.log("POST garden/create");
+
+    if (user.type != "admin") {
+        res.json({ message: "ERROR - this is an admin function" });
+        return;
+    }
+
+    await Garden.create({
+        number: req.body.number,
+        user: req.body.gardener || null
+    });
+
+    console.log("END POST garden/create");
+});
+
+/**
+ * req.body.user: User._id  // the person making the post
+ * req.body.number: Number
+ */
+ router.route("/delete").post(async (req, res) => {
+    console.log("POST garden/delete");
+
+    if (user.type != "admin") {
+        res.json({ message: "ERROR - this is an admin function" });
+        return;
+    }
+
+    let garden = await Garden.findOne({ number: req.body.number });
+
+    await garden.delete();
+
+    console.log("END POST garden/create");
+});
+
+/**
+ * req.body.user: User._id  // the person making the post
+ * req.body.number: Number
+ * req.body.gardener: User._id  // optional
+ */
+ router.route("/assign").post(async (req, res) => {
+    console.log("POST garden/assign");
+
+    if (user.type != "admin") {
+        res.json({ message: "ERROR - this is an admin function" });
+        return;
+    }
+
+    let garden = await Garden.findOne({ number: req.body.number });
+    garden.user = gardener;
+    garden.save();
+
+    console.log("END POST garden/assign");
+});
+
+/**
+ * req.body.user: User._id  // the person making the post
+ * req.body.number: Number
+ */
+ router.route("/unassign").post(async (req, res) => {
+    console.log("POST garden/delete");
+
+    if (user.type != "admin") {
+        res.json({ message: "ERROR - this is an admin function" });
+        return;
+    }
+
+    let garden = await Garden.findOne({ number: req.body.number });
+    garden.user = null;
+    garden.save();
+
+    console.log("END POST garden/create");
+});
+
 module.exports = router;
